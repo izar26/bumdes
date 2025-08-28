c4eaf58a289612d15efe6de29d55bc74978f6030
finn <finnnxptrix@gmail.com>
2025-08-28 08:15:05 +0700

    push

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -15,6 +15,6 @@ class AuthenticatedSessionController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
-        return redirect('/login');
+        return redirect('/');
     }
 }
